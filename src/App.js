95b457d32d910b5b8be32c0febe49ef4c665f116
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-07-19 14:48:44 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import image from "./images/Titanic_poster.jpg";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = [
+    {
+      title: "Titanic"
+    },
+    {
+      sinopse:
+        "A poor man and a rich young woman meet and fall in love on the voyage of the Titanic, in 1912. It will be from the arrogant heir of a steel mill, a young challenger to his family and friends in search of true love."
+    }
+  ];
+
+  render() {
+    return (
+      <div className="movie">
+        <h1>{this.state[0].title}</h1>
+        <p>{this.state[1].sinopse}</p>
+        <img src={image} alt="aaaaa" />
+      </div>
+    );
+  }
 }
-
-export default App;
